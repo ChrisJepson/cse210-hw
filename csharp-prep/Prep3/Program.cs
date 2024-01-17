30df24bb9f68e58f4c4739499db010e5e6e6f999
Christopher Jepson <jep20006@byui.edu>
2024-01-17 12:22:47 -0700

    Completed Prep3 Program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (number != guess)
+            {
+                Console.WriteLine("Guess a number from 1-100. What is your guess?");
+                string user_guess = Console.ReadLine();
+                guess = int.Parse(user_guess);
+
+                if (number > guess)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                else if (number < guess)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                else
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+            }
+
+
+        
+
+
     }
 }
