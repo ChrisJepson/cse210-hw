e5f5e4ebdf883a4b9aa5e960c530c9b274f8541e
Christopher Jepson <jep20006@byui.edu>
2024-01-17 23:41:53 -0700

    Completed Prep4 Program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int user_number = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (user_number != 0)
+            {
+                Console.Write("Enter a number: ");
+                string user_input = Console.ReadLine();
+                user_number = int.Parse(user_input);
+
+            if (user_number != 0)
+                {
+                    numbers.Add(user_number);
+                }
+
+            }
+        
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max_number = 0;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > max_number)
+            {
+                max_number = numbers[i];
+            }
+        }
+        Console.WriteLine($"The largest number is: {max_number}");
+        
+
     }
 }
