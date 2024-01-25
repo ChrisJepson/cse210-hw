d76527d2926c559e6f1e227d7f51f0a65b989e0e
Christopher Jepson <jep20006@byui.edu>
2024-01-24 21:27:25 -0700

    Completed Learning02 assignment codes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Del Taco";
+        job1._jobTitle = "Manager";
+        job1._startyear = 1706;
+        job1._endyear = 2020;
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Manager";
+        job2._startyear = 2020;
+        job2._endyear = 2024;
+
+        Resume r = new Resume();
+        r._name = "Christopher Jepson";
+        r._jobs.Add(job1);
+        r._jobs.Add(job2);
+        r.Display();
+
     }
 }
