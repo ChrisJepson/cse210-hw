072db1aeaa5869ba305f802fcd1ecf1f42ff28d0
Christopher Jepson <jep20006@byui.edu>
2024-02-22 00:00:21 -0700

    Finished prepare learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    static void Main(string[] args){
+
+        Assignment a1 = new Assignment("Christopher Jepson", "Addition");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Blake Ryan", "Division", "8.4", "5-12");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Ellie Jepson", "Health & Nutrition", "How to maintain your cardiovascular health");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
